ceeb8197ef79c4ca6bd330fbf81ed29b93275d71
Ahmed-matrix <119659334+qithoniq@users.noreply.github.com>
2023-07-04 09:23:58 +0300

    (#13)

--- matrix/helpers/utils/events.py
+++ matrix/helpers/utils/events.py
@@ -28,7 +28,7 @@ async def reply_id(event):
     return reply_to_id
 
 
-async def get_chatinfo(event, match, jmthonevent):
+async def get_chatinfo(event, match, matrix):
     if not match and event.reply_to_msg_id:
         replied_msg = await event.get_reply_message()
         if replied_msg.fwd_from and replied_msg.fwd_from.channel_id is not None:
