0deb94a24445e00f81dfd497c37f86f62f034057
Ahmed-matrix <119659334+qithoniq@users.noreply.github.com>
2023-07-04 13:37:45 +0300

    Update jikan.py

--- matrix/helpers/functions/jikan.py
+++ matrix/helpers/functions/jikan.py
@@ -359,8 +359,8 @@ async def formatJSON(outData, manga=False):
     msg += f"\n**السنة** : {jsonData['startDate']['year']}"
     msg += f"\n**المعدل** : {jsonData['averageScore']}"
     msg += f"\n**الشعبية** : {jsonData['popularity']}"
-    jmthon = f"{jsonData['description']}"
-    msg += " __" + re.sub("<br>", "\n", jmthon) + "__"
+    matrix = f"{jsonData['description']}"
+    msg += " __" + re.sub("<br>", "\n", matrix) + "__"
     msg = re.sub("<b>", "__**", msg)
     msg = re.sub("</b>", "**__", msg)
     return msg
