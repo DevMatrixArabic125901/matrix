dfb622d1af8e252533cb047207c7cbd92a7f03df
MaTrix <119659334+qithoniq@users.noreply.github.com>
2023-07-17 13:40:09 +0300

    Update iytdl.py

--- matrix/assistant/iytdl.py
+++ matrix/assistant/iytdl.py
@@ -32,7 +32,7 @@ BASE_YT_URL = "https://www.youtube.com/watch?v="
 YOUTUBE_REGEX = re.compile(
     r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})"
 )
-PATH = "./matrix/sql_Ahmed/ytsearch.json"
+PATH = "./matrix/sql_matrix/ytsearch.json"
 plugin_category = "bot"
 
 
