ea17d381abae1183ccf88665d5b7301293efe607
drago <119659334+qithoniq@users.noreply.github.com>
2023-08-22 03:28:54 +0300

    Update tools.py

--- matrix/helpers/tools.py
+++ matrix/helpers/tools.py
@@ -23,12 +23,12 @@ def media_type(message):
 
 async def post_to_telegraph(page_title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "MaTriX"
+    auth_name = "matrix"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=page_title,
         author=auth_name,
-        author_url="https://t.me/MaTriX_ThoN",
+        author_url="https://t.me/MaTriXThoN",
         text=html_format_content,
     )
     return post_page["url"]
