d14ce3a221db1b4db07f22e616e4c2f7adaa445b
Ahmed programmer <119659334+qithoniq@users.noreply.github.com>
2023-10-28 23:03:29 +0300

    Update matrix.py

--- matrix/plugins/matrix.py
+++ matrix/plugins/matrix.py
@@ -176,52 +176,6 @@ def convert_from_bytes(size):
 
 
 
-@matrix.on(admin_cmd(pattern=f"{ALIVE}(?: |$)(.*)"))     
-async def mat(matrixevent):
-    reply_to_id = await reply_id(matrixevent)
-    uptime = await get_readable_time((time.time() - StartTime))
-    start = datetime.now()
-    matrixevent = await edit_or_reply(matrixevent, "**᥀︙ جاري فحص السورس **")
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "᥀︙"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "𝖶𝖾𝗅𝖼𝗈𝗆 𝖬𝖺𝗍𝗋𝗂x 𝖠𝗋𝖺𝖻𝗂𝖼"
-    MATRIX_IMG = gvarstatus("ALIVE_PIC") or "https://telegra.ph/file/b180dcd0020f55cb63f8a.mp4"
-    tg_bot = Config.TG_BOT_USERNAME
-    me = await matrixevent.client.get_me()
-    my_last = me.last_name
-    my_mention = f"[{me.last_name}](tg://user?id={me.id})"
-    MATRIXTM = time.strftime("%I:%M")
-    matrixcaption = gvarstatus("ALIVE_MATRIXTMATRIXT") or matrix
-    caption = matrixcaption.format(        ALIVE_TEXT=ALIVE_TEXT,
-        EMOJI=EMOJI,
-        mention=mention,
-        uptime=uptime,
-        matrix=version.__version__,
-        catver=catversion,
-        pyver=python_version(),
-        dbhealth=check_sgnirts,
-        ping=ms,
-        my_mention=my_mention,
-        MATRIXTM=MATRIXTM,
-        tg_bot=tg_bot,    )
-    if MATRIX_IMG:
-        CAT = [x for x in MATRIX_IMG.split()]
-        PIC = random.choice(CAT)
-        try:
-            await matrixevent.client.send_file(matrixevent.chat_id, PIC, caption=caption, reply_to=reply_to_id)
-            await matrixevent.delete()
-        except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
-            return await edit_or_reply(matrixevent)
-    else:
-        await edit_or_reply(matrixevent,caption)
-matrix = """"‎᥀┊𝖬𝗒 𖠄 {my_mention} ٫
-‌‎᥀┊𝖳𝗂𝗆𝖾 𖠄 {MATRIXMATRIXTM} ٫
-‌‎᥀┊𝗎𝗉 𝖳𝗂𝗆𝖾 𖠄 {uptime} ٫
-‌‎᥀┊‌‎𝖯𝗂𝗇𝗀 𖠄 {ping} ٫
-‌‎᥀┊𝖡𝗈𝖳 𖠄 {tg_bot} ٫
-‌‎᥀┊‌‎𝖬𝖺𝖳𝗋𝗂x 𝖠𝗋𝖺𝖻𝗂𝖼 𖠄 @MaTrixThon"""
 
 @matrix.on(admin_cmd(pattern="حذف جميع الملفات(?: |$)(.*)"))    
 async def _(event):
@@ -230,6 +184,61 @@ async def _(event):
     OUTPUT = f"**تنبيـه، لقـد تم حـذف جميـع المجلـدات والملفـات الموجـودة في البـوت بنجـاح ✓**"
     event = await edit_or_reply(event, OUTPUT)
 	
+@matrix.ma_cmd(pattern="فحص(?:\s|$)([\s\S]*)")
+async def amireallyalive(event):
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    await edit_or_reply(event, "**᥀︙ جاري فحص السورس**")
+    end = datetime.now()
+    ping = (end - start).microseconds / 1000
+    _, check_sgnirts = check_data_base_heal_th()
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "᥀︙‎"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "𝖶𝖾𝗅𝖼𝗈𝗆 𝖬𝖺𝗍𝗋𝗂x 𝖠𝗋𝖺𝖻𝗂𝖼"
+    MATRIX_IMG = gvarstatus("ALIVE_PIC") or "https://telegra.ph/file/b180dcd0020f55cb63f8a.mp4"
+    MATRIXTM = time.strftime("%I:%M")
+    matrixiq_caption = gvarstatus("ALIVE_MATRIXTMATRIXT") or temp
+    caption = matrixiq_caption.format(
+        ALIVE_TEXT=ALIVE_TEXT,
+        EMOJI=EMOJI,
+        my_mention=my_mention,
+        uptime=uptime,
+        mention=mention,
+        telever=version.__version__,
+        catver=catversion,
+        pyver=python_version(),
+        dbhealth=check_sgnirts,
+        ping=ping,
+        MATRIXTM=MATRIXTM,
+        tg_bot=tg_bot,    )
+    )
+    if MATRIX_IMG:
+        matrix = [x for x in MATRIX_IMG.split()]
+        PIC = random.choice(matrix)
+        try:
+            await event.client.send_file(
+                event.chat_id, PIC, caption=caption, reply_to=reply_to_id
+            )
+            await event.delete()
+        except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
+            return await edit_or_reply(
+                event,
+                f"**الميـديا خـطأ **\nغـير الرابـط بأستـخدام الأمـر  \n `.اضف_فار ALIVE_PIC رابط صورتك`\n\n**لا يمـكن الحـصول عـلى صـورة من الـرابـط :-** `{PIC}`",
+            )
+    else:
+        await edit_or_reply(
+            event,
+            caption,
+        )
+
+
+temp = """᥀┊𝖬𝗒 𖠄 {my_mention} ٫
+‌‎᥀┊𝖳𝗂𝗆𝖾 𖠄 {MATRIXMATRIXTM} ٫
+‌‎᥀┊𝗎𝗉 𝖳𝗂𝗆𝖾 𖠄 {uptime} ٫
+‌‎᥀┊‌‎𝖯𝗂𝗇𝗀 𖠄 {ping} ٫
+‌‎᥀┊𝖡𝗈𝖳 𖠄 {tg_bot} ٫
+‌‎᥀┊‌‎𝖬𝖺𝖳𝗋𝗂x 𝖠𝗋𝖺𝖻𝗂𝖼 𖠄 @MaTrixThon"""
+
 @matrix.on(admin_cmd(pattern="المده(?: |$)(.*)"))    
 
 async def amireallyalive(event):
